0ade5daea49c6918feb72406c89015d96a0f2889
Syseka <zalupov.pupa@yandex.ru>
2022-03-08 12:30:12 +0400

    Make main func's. Base functional is done. Need to male SQL-load.

--- bot-button.py
+++ bot-button.py
@@ -9,36 +9,51 @@ from telebot import types
 bot = telebot.TeleBot(tokenbot.token)
 # импорт токена бота
 
-# cols = ('MATCH', 'TIP', 'ODDS', 'SCORE', 'RESULT')
-# print(len(cols))    # 5
-# print(cols[1])      # TIP
-
-cols = ['MATCH', 'TIP', 'ODDS', 'SCORE', 'RESULT']
-# print(len(cols))    # 5
-# print(cols[3])      # SCORE
-# lencols = len(cols)
+global list
+list=[]
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Начать запись.")
-    btn2 = types.KeyboardButton("Загрузка в БД.")
-    btn3 = types.KeyboardButton("Сброс значений.")
-    markup.add(btn1, btn2, btn3)
+    #btn1 = types.KeyboardButton("Начать запись.")
+    btn1 = types.KeyboardButton("Загрузка в БД.")
+    btn2 = types.KeyboardButton("Сброс значений.")
+    markup.add(btn1, btn2)
+    global list
+    list = []
 # три кнопки. Одна начинает запись, вторая отправляет данные в БД, третья - очищает введенные значения..
     bot.send_message(message.chat.id,
-                     text=f"Привет, {message.from_user.first_name}. Для передачи значений "
-                          f"необходимо нажать на соответствующую кнопку и ввести поочередно, "
-                          f"в каждом отдельном сообщении данные, где:"
+                     text=f"Привет, {message.from_user.first_name}. Значения передаются "
+                          f"в каждом отдельном сообщении, где:"
 f"""
-1 - DATE 
-2 - MATCH 
-3 - TIP 
-4 - ODDS 
-5 - SCORE 
-6 - RESULT                   
+1 - DATE
+2 - MATCH
+3 - TIP
+4 - ODDS
+5 - SCORE
+6 - RESULT
+
+Список сейчас пуст - {list}.
 """, reply_markup=markup)
 
-
+@bot.message_handler(content_types=["text"])
+def loadToDB(message):
+    txt = message.text
+    global list
+    if (message.text == "Сброс значений."):
+        list=[]
+        bot.send_message(message.chat.id, text=f"List del: {list}.")
+    elif (message.text == "Загрузка в БД."):
+        bot.send_message(message.chat.id, text=f"Data {list} load.")
+    elif len(list) < 5:
+        if txt == "Начать запись." or txt == "Загрузка в БД." or txt == "Сброс значений." or txt == "/start":
+            pass
+        else:
+            list.append(txt)
+            bmes = bot.reply_to(message, f"Получено {txt}.\n{len(list)} - {list}.")
+            bot.register_next_step_handler(bmes, loadToDB)
+    elif len(list) == 5:
+        list.append(txt)
+        bot.send_message(message.chat.id, text=f"Получено {list}.")
 
 bot.polling(none_stop=True)
