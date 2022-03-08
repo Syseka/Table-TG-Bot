804b12f06575d09292b57a926280825afea1f54b
Syseka <zalupov.pupa@yandex.ru>
2022-03-08 12:46:15 +0400

    Add comment. Bot is work.

--- bot-button.py
+++ bot-button.py
@@ -4,10 +4,10 @@ import tokenbot
 import telebot
 from telebot import types
 # import mysql-connector-python
-# Пока что без SQL, сперва с кнопками надо разобраться.
+# I need it soon.
 
 bot = telebot.TeleBot(tokenbot.token)
-# импорт токена бота
+# import bot-token
 
 global list
 list=[]
@@ -21,7 +21,7 @@ def start(message):
     markup.add(btn1, btn2)
     global list
     list = []
-# три кнопки. Одна начинает запись, вторая отправляет данные в БД, третья - очищает введенные значения..
+# Two button to work with array.
     bot.send_message(message.chat.id,
                      text=f"Привет, {message.from_user.first_name}. Значения передаются "
                           f"в каждом отдельном сообщении, где:"
@@ -35,6 +35,7 @@ f"""
 
 Список сейчас пуст - {list}.
 """, reply_markup=markup)
+# start-message
 
 @bot.message_handler(content_types=["text"])
 def loadToDB(message):
@@ -43,17 +44,23 @@ def loadToDB(message):
     if (message.text == "Сброс значений."):
         list=[]
         bot.send_message(message.chat.id, text=f"List del: {list}.")
+# That code clean array from data.
     elif (message.text == "Загрузка в БД."):
         bot.send_message(message.chat.id, text=f"Data {list} load.")
+# That code must load data to SQL BD,
+# i need to make it.
     elif len(list) < 5:
         if txt == "Начать запись." or txt == "Загрузка в БД." or txt == "Сброс значений." or txt == "/start":
             pass
+# Check, what data send by user.
         else:
             list.append(txt)
             bmes = bot.reply_to(message, f"Получено {txt}.\n{len(list)} - {list}.")
             bot.register_next_step_handler(bmes, loadToDB)
+# Write 5 values to array.
     elif len(list) == 5:
         list.append(txt)
         bot.send_message(message.chat.id, text=f"Получено {list}.")
+# Write last value and show all array.
 
 bot.polling(none_stop=True)
