7c598f6483f04fcf828b8c56539a218bf865d6fd
Syseka <zalupov.pupa@yandex.ru>
2022-03-10 21:02:23 +0400

    Add func's to connect and load data to DB.

--- bot-button.py
+++ bot-button.py
@@ -1,15 +1,34 @@
 # bot-button.py
 
-import tokenbot
+import config
+import mysql.connector
+from mysql.connector import Error
 import telebot
 from telebot import types
+from configDB import usH, usN, usP
+from connectToDB import addValue
 
-bot = telebot.TeleBot(tokenbot.token)
+bot = telebot.TeleBot(config.token)
 # import bot-token
+
+def connectDB(hostN, userN, userP):
+    global connection
+# its really need
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = hostN,
+            user = userN,
+            passwd = userP)
+        #print('Ok.')
+    except Error as e:
+        print(f'Error: {e}')
+
+    return connection
 
 global list
 list=[]
-
+connectDB(usH, usN, usP)
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -36,6 +55,7 @@ f"""
 # start-message
 
 @bot.message_handler(content_types=["text"])
+
 def loadToDB(message):
     txt = message.text
     global list
@@ -45,10 +65,18 @@ def loadToDB(message):
 # That code clean array from data.
     elif (message.text == "Загрузка в БД."):
         bot.send_message(message.chat.id, text=f"Data {list} load.")
-# That code must load data to SQL BD,
-# i need to make it.
+
+        addstr = f"""INSERT INTO `a0263496_bfb_stat_tgbot`.`TableTest` 
+        (`date`,`match`,`tip`,`odds`,`score`,`result`)
+        VALUES
+        ('{list[0]}','{list[1]}','{list[2]}','{list[3]}','{list[4]}', '{list[5]}')"""
+# Elements of array as values for SQL-request to DB.
+
+        addValue(connection, addstr)
+# Importing func to load values in table on DB
     elif len(list) < 5:
-        if txt == "Начать запись." or txt == "Загрузка в БД." or txt == "Сброс значений." or txt == "/start":
+        if txt == "Начать запись." or txt == "Загрузка в БД."\
+        or txt == "Сброс значений." or txt == "/start":
             pass
 # Check, what data send by user.
         else:
--- connectToDB.py
+++ connectToDB.py
@@ -3,18 +3,25 @@
 import mysql.connector
 from mysql.connector import Error
 from configDB import usH, usN, usP
-# import modules and DB log:pass from file
+    # import modules and DB log:pass from file
 
-#table = """CREATE TABLE `a0263496_bfb_stat_tgbot`.`Table1` (
-#  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-#  `date` VARCHAR(32) NOT NULL,
-#  `tip` VARCHAR(500) NOT NULL,
-#  `odds` VARCHAR(500) NOT NULL,
-#  `score` ENUM('w','l','m') NOT NULL,
-#  `result` ENUM('w','l','m') NOT NULL,
-#  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-#  PRIMARY KEY (`id`)
-#) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;"""
+table = """CREATE TABLE `a0263496_bfb_stat_tgbot`.`TableTest` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `date` VARCHAR(32) NOT NULL,
+  `match` VARCHAR(32) NOT NULL,
+  `tip` VARCHAR(50) NOT NULL,
+  `odds` VARCHAR(50) NOT NULL,
+  `score` VARCHAR(50) NOT NULL,
+  `result` VARCHAR(50) NOT NULL,
+  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;"""
+
+addstr = """INSERT INTO `a0263496_bfb_stat_tgbot`.`TableTest` 
+(`date`,`match`,`tip`,`odds`,`score`,`result`)
+VALUES
+('test1','test2','test3','test4','test5', 'test6')
+"""
 
 def connectDB(hostN, userN, userP):
     global connection
@@ -25,7 +32,7 @@ def connectDB(hostN, userN, userP):
             host = hostN,
             user = userN,
             passwd = userP)
-        print('Ok.')
+        #print('Ok.')
     except Error as e:
         print(f'Error: {e}')
 
@@ -33,13 +40,26 @@ def connectDB(hostN, userN, userP):
 
 connectDB(usH, usN, usP)
 
-#def creatDB_Table(connect, query):
+def addValue(connection,value):
+    connection
+    cursor = connection.cursor()
+    try:
+        cursor.execute(value)
+        connection.commit()
+        #print("Добавил.")
+    except Error as e:
+        print(f"Error: {e}")
+
+def creatDB_Table(connect, query):
 # Funk to make table in MySQL
-#    cursor = connect.cursor()
-#    try:
-#        cursor.execute(query)
-#        connect.commit()
-#        print("Таблица создана.")
-#    except Error as e:
-#        print(f"Error: {e}")
+    cursor = connect.cursor()
+    try:
+        cursor.execute(query)
+        connect.commit()
+        #print("Таблица создана.")
+    except Error as e:
+        print(f"Error: {e}")
+
 # creatDB_Table(connection, table)
+
+addValue(connection,addstr)
