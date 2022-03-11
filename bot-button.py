bfceae977b00dc26d3ec7d23747b01cca9814475
Syseka <zalupov.pupa@yandex.ru>
2022-03-11 18:34:37 +0400

    Added button to bot and made function for that, to show, what is in SQL.

--- bot-button.py
+++ bot-button.py
@@ -26,6 +26,21 @@ def connectDB(hostN, userN, userP):
 
     return connection
 
+request = """
+SELECT * FROM `a0263496_bfb_stat_tgbot`.`TableTest`
+"""
+
+def importValue(connection, req):
+    cursor = connection.cursor()
+    global result
+    result = None
+    try:
+        cursor.execute(req)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"Error: {e}")
+
 global list
 list=[]
 connectDB(usH, usN, usP)
@@ -34,8 +49,8 @@ def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Загрузка в БД.")
     btn2 = types.KeyboardButton("Сброс значений.")
-    #btn3 = types.KeyboardButton("Выгрузка из ДБ.")
-    markup.add(btn1, btn2)
+    btn3 = types.KeyboardButton("Выгрузка из ДБ.")
+    markup.add(btn1, btn2, btn3)
     global list
     list = []
 # Two button to work with array.
@@ -55,19 +70,36 @@ f"""
 # start-message
 
 @bot.message_handler(content_types=["text"])
-
 def loadToDB(message):
     txt = message.text
     global list
+
+# That code clean array from data.
     if (message.text == "Сброс значений."):
         list=[]
         bot.send_message(message.chat.id, text=f"List del: {list}.")
-# That code clean array from data.
-    elif (message.text == "Загрузка в БД."):
 
-        bot.send_message(message.chat.id, text=f"Data: ")
-# Show, what in DB.
-# I need do it soon.
+    # Show, what in DB.
+    elif (message.text == "Выгрузка из ДБ."):
+        importValue(connection, request)
+        for data in result:
+# RESULT now is double array => result [data] [value]
+
+# There bot send 1 message with 1 string from DB
+# Yes, i need to make a limit about than.
+            bot.send_message(message.chat.id, text=f"""
+ЗАПИСЬ - {data[0]}
+ 
+DATE - {data[1]}
+MATCH - {data[2]}
+TIP - {data[3]}
+ODDS - {data[4]}
+SCORE - {data[5]}
+RESULT - {data[6]}
+TIME - {data[7]}
+""")
+
+    # Importing func to load values in table on DB
     elif (message.text == "Загрузка в БД."):
         bot.send_message(message.chat.id, text=f"Data {list} load.")
 
@@ -78,7 +110,7 @@ def loadToDB(message):
 # Elements of array as values for SQL-request to DB.
 
         addValue(connection, addstr)
-# Importing func to load values in table on DB
+
     elif len(list) < 5:
         if txt == "Начать запись." or txt == "Загрузка в БД."\
         or txt == "Сброс значений." or txt == "/start":
